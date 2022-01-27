bad945559916779e768d40d4d65b35d1962a0183
Braeden <clarke980@gmail.com>
2022-01-27 01:33:11 -0600

    Added live processing of regex
    Made these changes to display a live counter of valid URLs later. Makes it easier for the user to see if there is a glaring issue in their regex formatting

--- popup/save_urls.js
+++ popup/save_urls.js
@@ -1,43 +1,72 @@
+let urls = [];
+var refined_urls = [];
+let prev_regex_len = 0;
+
+function update_urls(tabs){
+    for (const tab of tabs){
+        urls.push(tab.url);
+    }
+    refined_urls = urls;
+}
+
+function handle_regex_update(event){
+    if (!event.target.classList.contains("regex-input")){
+        return;
+    }
+    
+    if (!(event.target.value.length > prev_regex_len)){
+        reset_refined_urls();
+    }
+
+    refine_urls(event.target.value);
+    prev_regex_len = event.target.value.length;
+
+    //Update number indicator
+}
+
+function reset_refined_urls(){
+    refined_urls = urls.slice(0);
+}
+
+function refine_urls(regex_string){
+    let tmpArray = [];
+    let regex = new RegExp(regex_string);
+    for (const url of refined_urls){
+        if (regex.test(url))
+            tmpArray.push(url);
+    }
+    refined_urls = tmpArray;
+}
+
 function handleClick(e){
     if (!e.target.classList.contains("save-button"))
         return;
     
-    browser.tabs.query({ currentWindow: true }).then(processTabs, tabsError);
+    save_urls();
+    e.preventDefault();
 }
 
-function processTabs(tabs){
-    let urlArray = []
-    let regex = new RegExp(document.getElementById('regex-input').value);
-    for (const tab of tabs){
-        let url = tab.url;
-        if (regex.test(url)){
-            urlArray.push(url);
-        }
-    }
-    writeArrayToFile(urlArray);
-}
 
-function tabsError(error){
-    console.log(error);
-}
-
-function writeArrayToFile(array){
-    if (array.length === 0){
+function save_urls(){
+    if (refined_urls.length === 0){
+        //Do more later
         return;
     }
 
     let text = "";
     let filename = "urls.txt";
-    for (const url of array){
+    for (const url of refined_urls){
         text += url + "\n";
     }
 
-    var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:text/plain;charset=urt-8,' + encodeURIComponent(text));
-    pom.setAttribute('download', filename);
-    pom.click();
+    var pawn = document.createElement('a');
+    pawn.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pawn.setAttribute('download', filename);
+    pawn.click();
 }
 
+browser.tabs.query({ currentWindow: true }).then(update_urls);
+document.addEventListener("input", handle_regex_update);
 document.addEventListener("click", handleClick);
 const input = document.getElementById("regex-input");
 input.addEventListener("keyup", function(event){
